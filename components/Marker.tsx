deaba8c2289bb84368bfa39e287edea922d1a9a0
Kuba Kaczmarek <jakub.kaczmarek@tpa-group.pl>
2024-01-11 10:18:36 +0100

    Update marker visually

--- components/Marker.tsx
+++ components/Marker.tsx
@@ -2,12 +2,12 @@ import { cn } from '@/lib/utils'
 import { VariantProps, cva } from 'class-variance-authority'
 import React from 'react'
 
-const button = cva('absolute w-[15px] h-[15px] md:w-[25px] md:h-[25px] lg:w-[35px] lg:h-[35px] -translate-x-1/2 -translate-y-1/2 rounded-full border-primary border-4 pointer-events-none', {
+const button = cva('absolute w-[15px] h-[15px] -translate-x-1/2 -translate-y-1/2 rounded-full border-primary border-4 pointer-events-none', {
     variants: {
         color: {
             primary: 'border-primary',
             secondary: 'border-secondary',
-            tertiary: 'border-tertiary',
+            tertiary: 'border-green-400',
         },
     }
 })
