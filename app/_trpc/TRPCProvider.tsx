c65dadea20d527d408c99661bbb934b5dc59bdc4
Kuba Kaczmarek <jakub.kaczmarek@tpa-group.pl>
2024-01-12 10:12:33 +0100

    Setup trpc for Vercel deployment

--- app/_trpc/TRPCProvider.tsx
+++ app/_trpc/TRPCProvider.tsx
@@ -6,13 +6,26 @@ import React, { useState } from 'react'
 
 import { trpc } from './client'
 
+function getBaseUrl() {
+    if (typeof window !== 'undefined')
+      // browser should use relative path
+      return '';
+  
+    if (process.env.VERCEL_URL)
+        // reference for vercel.com
+        return `https://${process.env.VERCEL_URL}`;
+
+    // assume localhost
+    return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export default function TRPCProvider({ children }: { children: React.ReactNode }) {
     const [queryClient] = useState(() => new QueryClient({}))
     const [trpcClient] = useState(() => 
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: 'http://localhost:3000/api/trpc',
+                    url: `${getBaseUrl()}/api/trpc`,
                 })
             ]
         })
