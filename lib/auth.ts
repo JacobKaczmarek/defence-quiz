732dab41eb80655d7e85ebdc6deabad55cf21268
Kuba Kaczmarek <jakub.kaczmarek@tpa-group.pl>
2024-01-16 20:09:29 +0100

    Add discord provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from 'next-auth';
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import FacebookProvider from 'next-auth/providers/facebook';
+import DiscordProvider from 'next-auth/providers/discord';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
@@ -8,9 +9,13 @@ const prisma = new PrismaClient()
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID as string,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    // }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
     }),
   ],
   adapter: PrismaAdapter(prisma),
